92112bc7f875cd0f32409edec84fd279345f6211
RHRiday <iiucrifat@gmail.com>
2021-06-27 18:53:51 +0600

    add remote db

--- config/config.php
+++ config/config.php
@@ -2,9 +2,18 @@
 ob_start(); //Turns on output buffering 
 session_start();
 
-$timezone = date_default_timezone_set("Europe/London");
+$timezone = date_default_timezone_set("Asia/Dhaka");
 
-$con = mysqli_connect("localhost", "root", "", "social"); //Connection variable
+//Get Heroku Remote connection information
+$remotedb_url = parse_url(getenv("REMOTE_DB"));
+$remotedb_server = $remotedb_url["host"];
+$remotedb_username = $remotedb_url["user"];
+$remotedb_password = $remotedb_url["pass"];
+$remotedb_db = substr($remotedb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);  //Connection variable
 
 if(mysqli_connect_errno()) 
 {
